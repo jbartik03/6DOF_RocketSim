d94a95005c78ed00db713b0c9958fe5cb168c3ae
jbartik03 <jbbassoon03@gmail.com>
2026-07-14 10:08:17 -0400

    Delete groundEvent.m

--- groundEvent.m
+++
@@ -1,5 +0,0 @@
-function [value,isterminal,direction] = groundEvent(~,x)
-value = x(1)-1;    % velocity
-isterminal = 1;    % stop integration
-direction = -1;    % detect crossing from up to down
-end
